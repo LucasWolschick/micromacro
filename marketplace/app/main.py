1313e8cfd29aee19ed7d5887bcc9890b8a7bb602
Lucas Wolschick <6264382+LucasWolschick@users.noreply.github.com>
2025-11-09 11:51:33 -0300

    add marketplace service

---
+++ marketplace/app/main.py
@@ -0,0 +1,36 @@
+import logging
+from typing import Awaitable, Callable
+from fastapi import FastAPI, Request, Response
+from fastapi.concurrency import asynccontextmanager
+from fastapi.responses import JSONResponse
+
+from common.db.connection import ConnectionPool
+from common.db.deps import set_db_instance
+
+from app.exceptions import ApplicationException
+from app.settings import settings
+
+db = ConnectionPool(settings.connection_string, settings.db_name)
+set_db_instance(db)
+
+logging.basicConfig(level=settings.log)
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await db.connect()
+    yield
+    await db.disconnect()
+
+
+app = FastAPI(lifespan=lifespan)
+
+
+@app.middleware("http")
+async def handle_exceptions(
+    request: Request, call_next: Callable[[Request], Awaitable[Response]]
+) -> Response:
+    try:
+        return await call_next(request)
+    except ApplicationException as e:
+        return JSONResponse(status_code=400, content={"message": str(e)})
---
+++ marketplace/app/settings.py
@@ -0,0 +1,19 @@
+import logging
+import os
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+ENV = os.getenv("ENV", "dev")
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=f".env.{ENV}", env_file_encoding="utf-8", case_sensitive=False
+    )
+
+    connection_string: str
+    db_name: str
+
+    log: int | str = logging.INFO
+
+
+settings = Settings()  # type: ignore
