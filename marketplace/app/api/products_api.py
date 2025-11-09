a49ef0d923aee35a0d56e465b6c79a4a94699976
Lucas Wolschick <6264382+LucasWolschick@users.noreply.github.com>
2025-11-09 16:01:27 -0300

    add get_product use case to marketplace service

--- marketplace/app/api/products_api.py
+++ marketplace/app/api/products_api.py
@@ -4,6 +4,7 @@ from httpx import AsyncClient
 
 from app.clients.deps import get_http_client
 from app.use_cases.list_products import ListProducts
+from app.use_cases.get_product import GetProduct
 from app.clients.client_factory import ClientFactory
 
 
@@ -15,3 +16,12 @@ async def list_products(api_client: Annotated[AsyncClient, Depends(get_http_clie
     factory = ClientFactory(api_client)
     use_case = ListProducts(factory.catalog(), factory.inventory())
     return await use_case.run()
+
+
+@router.get("/products/{id}")
+async def get_product(
+    api_client: Annotated[AsyncClient, Depends(get_http_client)], product_id: int
+):
+    factory = ClientFactory(api_client)
+    use_case = GetProduct(factory.catalog(), factory.inventory())
+    return await use_case.run(product_id)
---
+++ marketplace/app/use_cases/get_product.py
@@ -0,0 +1,69 @@
+from decimal import Decimal
+from pydantic import BaseModel
+from app.clients.catalog_client import CatalogClient
+from app.clients.inventory_client import InventoryClient, ListStocksRequest
+
+
+class WarehouseProductStock(BaseModel):
+    warehouse: int
+    quantity: Decimal
+
+
+class ProductStock(BaseModel):
+    total_quantity: Decimal
+    deposits: list[WarehouseProductStock]
+
+
+class GetProductResponse(BaseModel):
+    sku: int
+    description: str
+    price: Decimal
+    stock: ProductStock
+
+
+class GetProduct:
+    def __init__(
+        self, catalog_client: CatalogClient, inventory_client: InventoryClient
+    ):
+        self.catalog_client = catalog_client
+        self.inventory_client = inventory_client
+
+    async def run(self, product_id: int) -> GetProductResponse:
+        product = await self.catalog_client.get_product(product_id)
+        stocks = await self.inventory_client.list_stocks(
+            ListStocksRequest(products=[product_id], warehouse_id=None)
+        )
+
+        stocksFromProduct: dict[int, list[WarehouseProductStock]] = {}
+        totalProductStock: dict[int, Decimal] = {}
+
+        for warehouse, productStocks in stocks.items():
+            for productStock in productStocks:
+                if productStock.stock.is_zero():
+                    continue
+
+                if productStock.product_id not in stocksFromProduct:
+                    stocksFromProduct[productStock.product_id] = []
+
+                if productStock.product_id not in totalProductStock:
+                    totalProductStock[productStock.product_id] = Decimal("0")
+
+                stocksFromProduct[productStock.product_id].append(
+                    WarehouseProductStock(
+                        warehouse=warehouse, quantity=productStock.stock
+                    )
+                )
+
+                totalProductStock[productStock.product_id] += productStock.stock
+
+        response = GetProductResponse(
+            sku=product.id,
+            description=product.description,
+            price=product.price,
+            stock=ProductStock(
+                total_quantity=totalProductStock.get(product.id, Decimal("0")),
+                deposits=stocksFromProduct.get(product.id, []),
+            ),
+        )
+
+        return response
